98c23140f19460a768cc5916a3a5201e5c3e5b56
Dan Iverson <dan@daniverson.com>
2017-05-17 21:35:17 -0500

    Remove Atlas post-processor

--- scripts/provision-psadmin.ps1
+++ scripts/provision-psadmin.ps1
@@ -1,5 +1,5 @@
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
-choco install jdk7 -y
+# choco install jdk7 -y
 choco install git -params '"/GitAndUnixToolsOnPath"' -y
 choco install procexp -y
 choco install beyondcompare -y
@@ -12,5 +12,5 @@ choco install jq -y
 If (Test-Path Alias:wget) {Remove-Item Alias:wget}
 If (Test-Path Alias:wget) {Remove-Item Alias:wget}
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
-git clone https://github.com/MarisElsins/getMOSPatch.git c:/psft/tools/getMOSPatch
+# git clone https://github.com/MarisElsins/getMOSPatch.git c:/psft/tools/getMOSPatch
 Set-ExecutionPolicy Unrestricted
