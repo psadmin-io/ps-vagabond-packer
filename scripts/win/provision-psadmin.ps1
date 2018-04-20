74dc6a7df0c8f29e1a647c760d10c52b93000df2
Dan Iverson <dan@daniverson.com>
2018-04-20 13:59:44 -0500

    Installing chocolatey before using choco command

--- scripts/win/provision-psadmin.ps1
+++ scripts/win/provision-psadmin.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 choco install git -params '"/GitAndUnixToolsOnPath"' -y
 choco install procexp -y
